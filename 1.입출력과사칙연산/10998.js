b01e66797b7f3c6a68f2eed4c462e95fa8409352
Jong jae Park <pjj186@naver.com>
2021-08-09 09:36:06 +0900

    10998

---
+++ 1.입출력과사칙연산/10998.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+let input = fs.readFileSync('예제입력.txt').toString().split('\n');
+
+let count = input[0];
+let numbers = [];
+let num1 = null;
+let num2 = null;
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] !== '') {
+    numbers.push(input[i].split(' '));
+  }
+}
+
+for (let i = 0; i < numbers.length; i++){
+  num1 = Number(numbers[i][0]);
+  num2 = Number(numbers[i][1]);
+
+}
+console.log(num1 * num2);
